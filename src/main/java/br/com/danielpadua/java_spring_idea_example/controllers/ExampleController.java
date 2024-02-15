cb8da53eeefdceef2f950068add155a6f0c7ef03
sundeepkumar <sundeep@sundeekumar.com>
2024-02-15 16:20:51 -0500

    added another comment

--- src/main/java/br/com/danielpadua/java_spring_idea_example/controllers/ExampleController.java
+++ src/main/java/br/com/danielpadua/java_spring_idea_example/controllers/ExampleController.java
@@ -13,10 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/example")
+//this is a controller 
 public class ExampleController {
 
     @GetMapping("/hello-world")
-    //fixed issue with branch 
+    //fixed issue with branch
     public ResponseEntity<String> get() {
         return ResponseEntity.ok("Hello  World!");
     }
